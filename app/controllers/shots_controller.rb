76e7e183f975fa87a3ea0d2bbf164ce0b2a6321b
christian <christiandavidphoto@gmail.com>
2018-05-10 14:59:33 -0400

    refactor

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,10 +1,11 @@
 class ShotsController < ApplicationController
   before_action :set_shot, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
   # GET /shots
   # GET /shots.json
   def index
-    @shots = Shot.all
+    @shots = Shot.all.order("created_at DESC")
   end
 
   # GET /shots/1
@@ -14,7 +15,7 @@ class ShotsController < ApplicationController
 
   # GET /shots/new
   def new
-    @shot = Shot.new
+    @shot = current_user.shots.build
   end
 
   # GET /shots/1/edit
@@ -24,11 +25,11 @@ class ShotsController < ApplicationController
   # POST /shots
   # POST /shots.json
   def create
-    @shot = Shot.new(shot_params)
+    @shot = current_user.shots.build(shot_params)
 
     respond_to do |format|
       if @shot.save
-        format.html { redirect_to @shot, notice: 'Shot was successfully created.' }
+        format.html { redirect_to @shot, notice: "Shot was successfully created." }
         format.json { render :show, status: :created, location: @shot }
       else
         format.html { render :new }
@@ -42,7 +43,7 @@ class ShotsController < ApplicationController
   def update
     respond_to do |format|
       if @shot.update(shot_params)
-        format.html { redirect_to @shot, notice: 'Shot was successfully updated.' }
+        format.html { redirect_to @shot, notice: "Shot was successfully updated." }
         format.json { render :show, status: :ok, location: @shot }
       else
         format.html { render :edit }
@@ -56,19 +57,20 @@ class ShotsController < ApplicationController
   def destroy
     @shot.destroy
     respond_to do |format|
-      format.html { redirect_to shots_url, notice: 'Shot was successfully destroyed.' }
+      format.html { redirect_to shots_url, notice: "Shot was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_shot
-      @shot = Shot.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def shot_params
-      params.require(:shot).permit(:title, :description, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_shot
+    @shot = Shot.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def shot_params
+    params.require(:shot).permit(:title, :description, :user_id)
+  end
 end
