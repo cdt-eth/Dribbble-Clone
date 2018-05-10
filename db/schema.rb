dae0bf60325cd2b1cf65b7e975a80a3d11b4ab6b
christian <christiandavidphoto@gmail.com>
2018-05-10 14:30:01 -0400

    comment controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_10_172807) do
+ActiveRecord::Schema.define(version: 2018_05_10_182843) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "response"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shots", force: :cascade do |t|
     t.string "title"
