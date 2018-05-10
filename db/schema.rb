c0715dd5918e410b0dada7af95b8eca4ceabc7a8
christian <christiandavidphoto@gmail.com>
2018-05-10 14:33:23 -0400

    add user id to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_10_182843) do
+ActiveRecord::Schema.define(version: 2018_05_10_183223) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "response"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "shot_id"
+    t.integer "user_id"
   end
 
   create_table "shots", force: :cascade do |t|
